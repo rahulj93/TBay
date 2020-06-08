eeff6f50a2d718504f9f2a6353006fb4cf1dba50
Rahul Jain <rahulj93@gmail.com>
2020-06-08 11:51:07 -0400

    Added styling to the session form

--- frontend/components/session_form/session_form.jsx
+++ frontend/components/session_form/session_form.jsx
@@ -4,9 +4,20 @@ import { Link } from 'react-router-dom';
 class SessionForm extends React.Component {
 	constructor(props) {
 		super(props)
-		this.state = {username: '', password: ''}
+		this.state = {firstname: '', lastname: '', username: '', password: '', disabled:true}
 		this.handleSubmit = this.handleSubmit.bind(this); 
 	}  
+
+	componentDidUpdate () {
+		Object.values(this.state).forEach (prop=>{
+			// if (prop === "" && this.state.disabled ) {
+			// 	this.setState({disabled: false})
+			// } 
+			if (prop.length && this.state.disabled ) {
+				this.setState({disabled: false})
+			} 
+		})
+	}
 
 	update(field) {
 		return e => this.setState({
@@ -20,11 +31,18 @@ class SessionForm extends React.Component {
 		this.props.processForm(user); 
 	}
 
+	// info() {
+	// 	if (this.props.formType === 'signup') {
+			
+	// 	}
+	// }
+
+
 	render () {
 		return (
 			<div className="login-form-container">
 				<span className="logo">
-					<img width="250" height="100" alt="tBay Logo" src='assets/logo3.png' />
+					<Link to="/"> <img width="250" height="100" alt="tBay Logo" src='assets/logo3.png' /></Link>
 				</span>
 				<form onSubmit={this.handleSubmit} className="login-form-box">
 					{/* Create your Tbay account here  */}
@@ -38,25 +56,45 @@ class SessionForm extends React.Component {
 					<br/>
 					<div className="login-form">
 						<br />
-						<label> Username:  
+						<div className="name-textbox">
 							<input type="text"
-								value={this.state.username}
-								onChange={this.update('username')}
+								value={this.state.firstname}
+								onChange={this.update('firstname')}
 								className="login-input"
+								// placeholder="First name: "
 							/>
-						</label>
-						<br />
-						<br />
-						<label> Password:  
-							<input type="password"
-								value={this.state.password}
-								onChange={this.update('password')}
+							<label> First name: </label>
+            &nbsp;
+							<input type="text"
+								value={this.state.lastname}
+								onChange={this.update('lastname')}
 								className="login-input"
+								// placeholder='Last name:'
 							/>
-						</label>
+							<label className='lastname-label lastname'> Last name: </label>
+						</div>
+						<br/>
+
+						<div className="textbox">
+								<input type="text"
+									value={this.state.username}
+									onChange={this.update('username')}
+									className="login-input"
+									/>
+								<label> Email or username: </label>  
+						</div>
+						<br />
+						<div className="textbox">
+								<input type="password"
+									value={this.state.password}
+									onChange={this.update('password')}
+									className="login-input"
+									/>
+								<label> Password: </label> 
+						</div>
 						<br/> 
 						<br/> 
-						<button> {this.props.formType.toUpperCase()} </button>
+						<button disabled={this.state.disabled}> {this.props.formType.toUpperCase()} </button>
 					</div>
 				</form>
 			</div>
